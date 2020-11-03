a3e311243cae82075db697415f1cf9c376a834d4
Artur Galimskii <artur.galimskii@varwin.com>
2020-11-03 18:36:51 +0500

    Initial

---
+++ Assets/AnimalTopGun/Scripts/WeaponDirection.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace AnimalTopGun.Scripts
+{
+    public class WeaponDirection : MonoBehaviour
+    {
+        public Transform FrontPoint;
+        public Transform BackPoint;
+        public Transform TouchPoint;
+
+        public Vector3 GetDirectionNormalized()
+        {
+            return (FrontPoint.position - BackPoint.position).normalized;            
+        }
+    }
+}
